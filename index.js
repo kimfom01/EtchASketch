620a65f311a5129193d584a889de33037f4b0e58
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-02-09 15:28:20 +0300

    Create a grid of divs

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 console.log("Hello World!");
+
+function createDivs(widthHeight = 30, size = 16) {
+    let container = document.getElementById("container");
+    container.style.width = `${size * widthHeight}px`;
+    container.style.height = `${size * widthHeight}px`;
+
+    for (let row = 0; row < size; row++) {
+        for (let col = 0; col < size; col++) {
+            let div = document.createElement("div");
+            div.style.width = `${widthHeight}px`
+
+            div.innerText = row + col;
+
+            container.append(div);
+        }
+    }
+}
+
+createDivs();
